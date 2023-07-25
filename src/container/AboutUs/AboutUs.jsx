26eb0048d3e2f8021609bb3ae2e2f2a8973f3e86
rajneeshsingh18 <rajneeshsingh9448@gmail.com>
2023-07-25 21:29:12 +0530

    Header and AboutUS

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -2,9 +2,46 @@ import React from 'react';
 
 import './AboutUs.css';
 
+import { images } from '../../constants';
+
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id="about">
+    
+    <div className='app__aboutus-overlay flex__center'>
+
+      <img src={images.G} alt = "g letter"/>
+
+    </div>
+
+    <div className="app__aboutus-content flex__center"> 
+      <div className="app_aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src = {images.spoon} alt="spoon_img" className='spoon__image'/>
+        <p className="p__opensans">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.
+        </p>
+
+        <button type='button' className='custom__button'>
+            Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt=" about_knife" />
+      </div>
+
+      <div className="app_aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src = {images.spoon} alt="spoon_img" className='spoon__image'/>
+        <p className="p__opensans">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.
+        </p>
+
+        <button type='button' className='custom__button'>
+            Know More
+        </button>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 
 import './Header.css';
 
+import {images} from '../../constants'
+import { SubHeading } from '../../components';
+
+
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app_wrapper section__padding' id="home">
+        <div className="app__wrapper_info">
+            <SubHeading title= "Chase the new flavor"/>
+
+            <h1 className="app__header-h1">The key to Fine Dining</h1>
+
+            <p className="p__opensans" style={{margin : '2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+
+            <button className="custom__button">
+              Explore Menu
+            </button>
+        </div>
+
+        <div className="app__wrapper_img">
+          <img  src = {images.welcome} alt="header img"/>
+        </div>
   </div>
 );
 
